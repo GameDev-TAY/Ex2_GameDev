c34c3e04f2c29d828a832c81bb8dafffec173dac
Ariel Duek <arielduek@gmail.com>
2020-11-01 00:59:59 +0200

    Oscillator , Rotator and Hider are finished

---
+++ Assets/scripts/Hider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hider : MonoBehaviour
+{
+    private Renderer rend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H)){
+            rend.enabled = !rend.enabled;
+        }
+    }
+}
---
+++ Assets/scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ *  This component moves its object right and left like an Oscillator by given speed and range.
+ */
+public class Oscillator : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+    [SerializeField]
+    float range = 15f;
+
+    [SerializeField]
+    float speed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position; //Get the start position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPosition + new Vector3(Mathf.Sin((speed / range) * Time.time) * range, 0.0f, 0.0f); //use sin func to change position by given speed and range
+    }
+}
