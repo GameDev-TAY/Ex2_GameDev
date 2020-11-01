62ad23a300c751f472434042ca26e53375fb74f6
yiratpeleg <yiratpeleg8@gmail.com>
2020-11-01 21:32:54 +0200

    add beat script

---
+++ ExA/Assets/scripts/beat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * This component enlarges and reduce it's object like a beating heart.
+ */
+public class beat : MonoBehaviour
+{
+    private Vector3 startScale;
+
+    [SerializeField]
+    float range = 0.3f;
+
+    [SerializeField]
+    float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float toChange = (Mathf.Sin((speed / range) * Time.time) * range);
+        transform.localScale = startScale + new Vector3(toChange, toChange, 0.0f );
+    }
+}
