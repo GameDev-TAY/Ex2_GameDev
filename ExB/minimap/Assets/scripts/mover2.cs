5b5d581d221fef4d0bc9bb59d624313df68cf436
yiratpeleg <yiratpeleg8@gmail.com>
2020-11-02 22:02:02 +0200

    TwoPlayer done

---
+++ ExB/minimap/Assets/scripts/mover2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover2 : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
